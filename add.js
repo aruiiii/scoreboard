ddb27f07e996df42754bdb84e05cad5a674e68d4
aruiiii <1600321060@qq.com>
2026-04-10 02:48:23 +0800

    The first edition

---
+++ add.js
@@ -0,0 +1,26 @@
+// 主队加分函数
+function homeAddOne() {
+    document.getElementById("homeTeam").textContent = parseInt(document.getElementById("homeTeam").textContent) + 1;
+}       
+
+function homeAddTwo() {
+    document.getElementById("homeTeam").textContent = parseInt(document.getElementById("homeTeam").textContent) + 2;
+}
+
+function homeAddThree() {
+    document.getElementById("homeTeam").textContent = parseInt(document.getElementById("homeTeam").textContent) + 3;
+}
+
+// 客队加分函数
+function guestAddOne() {
+    document.getElementById("guestTeam").textContent = parseInt(document.getElementById("guestTeam").textContent) + 1;
+}
+
+function guestAddTwo() {
+    document.getElementById("guestTeam").textContent = parseInt(document.getElementById("guestTeam").textContent) + 2;
+}
+
+function guestAddThree() {
+    document.getElementById("guestTeam").textContent = parseInt(document.getElementById("guestTeam").textContent) + 3;
+}
+
